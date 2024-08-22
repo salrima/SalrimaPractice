bb11b5f9be0a71fb457774b8024df9d0c59b4769
salrima <salrima2001@gmail.com>
2024-08-22 16:44:51 +0530

    hellow world file added

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,10 @@ public class Calculator{
         result = a - b;
         return result;
     }
+    public double multiply(double a, double b) {
+        result = a * b;
+        return result;
+    }
    
     public static void main(String[] args) {
         Calculator calc = new Calculator();
@@ -15,6 +19,7 @@ public class Calculator{
         // Adding numbers
         System.out.println("Addition Result: " + calc.add(5, 3));
         System.out.println("Subtraction Result: " + calc.subtract(10, 4));
+        System.out.println("Multiplication Result: " + calc.multiply(2, 7));
     
     }
     
